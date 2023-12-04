3589d5bf27837cc46a9cb6658c7703b6d9c7f3ea
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-04 18:53:45 +0700

    Completed TextFieldsScreen coding

--- UXWithMaterial3/app/src/main/java/com/nguyenhoangthanhan/uxwithmaterial3/ui/screens/HomeScreen.kt
+++ UXWithMaterial3/app/src/main/java/com/nguyenhoangthanhan/uxwithmaterial3/ui/screens/HomeScreen.kt
@@ -2,12 +2,15 @@ package com.nguyenhoangthanhan.uxwithmaterial3.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.nguyenhoangthanhan.uxwithmaterial3.navigation.MaterialAppRouter
 import com.nguyenhoangthanhan.uxwithmaterial3.navigation.Screen
 
@@ -26,6 +29,8 @@ fun HomeScreen(){
         ) {
             Text(text = "Open Button Screen")
         }
+
+        Spacer(modifier = Modifier.size(16.dp))
 
         Button(
             onClick = {
--- UXWithMaterial3/app/src/main/java/com/nguyenhoangthanhan/uxwithmaterial3/ui/screens/TextFieldsScreen.kt
+++ UXWithMaterial3/app/src/main/java/com/nguyenhoangthanhan/uxwithmaterial3/ui/screens/TextFieldsScreen.kt
@@ -3,8 +3,15 @@ package com.nguyenhoangthanhan.uxwithmaterial3.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
 import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MonitorWeight
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,6 +20,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import com.nguyenhoangthanhan.uxwithmaterial3.navigation.MaterialAppRouter
 import com.nguyenhoangthanhan.uxwithmaterial3.navigation.Screen
@@ -20,14 +29,14 @@ import com.nguyenhoangthanhan.uxwithmaterial3.navigation.SystemBackButtonHandler
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextFieldsScreen(){
+fun TextFieldsScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         var filledText by remember {
-            mutableStateOf("abcdef")
+            mutableStateOf("")
         }
         TextField(
             value = filledText,
@@ -36,7 +45,66 @@ fun TextFieldsScreen(){
             },
             textStyle = LocalTextStyle.current.copy(
                 textAlign = TextAlign.Right
+            ),
+            placeholder = {
+                Text(text = "Weight")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.MonitorWeight,
+                    contentDescription = "Weight"
+                )
+            },
+            supportingText = {
+                Text(text = "*Required")
+            },
+            isError = false,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    println("Hello world!")
+                }
             )
+
+        )
+
+        var outlinedText by remember {
+            mutableStateOf("")
+        }
+        OutlinedTextField(
+            value = outlinedText,
+            onValueChange = {
+                filledText = it
+            },
+            textStyle = LocalTextStyle.current.copy(
+                textAlign = TextAlign.Right
+            ),
+            placeholder = {
+                Text(text = "Weight")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.MonitorWeight,
+                    contentDescription = "Weight"
+                )
+            },
+            supportingText = {
+                Text(text = "*Required")
+            },
+            isError = false,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    println("Hello world!")
+                }
+            )
+
         )
     }
 
