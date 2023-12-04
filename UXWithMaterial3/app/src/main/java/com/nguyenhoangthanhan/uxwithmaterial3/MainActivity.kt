86ac2dae66cca21f48a3376b61da8c4cc14a4304
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-04 16:50:57 +0700

    Added and completed coding buttons

--- UXWithMaterial3/app/src/main/java/com/nguyenhoangthanhan/uxwithmaterial3/MainActivity.kt
+++ UXWithMaterial3/app/src/main/java/com/nguyenhoangthanhan/uxwithmaterial3/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.nguyenhoangthanhan.uxwithmaterial3
 
+import android.graphics.Outline
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nguyenhoangthanhan.uxwithmaterial3.ui.theme.UXWithMaterial3Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +35,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UXWithMaterial3Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(
+                            onClick = {
 
+                            }
+                        ) {
+                            Text(text = "Create a new account")
+                        }
+                        OutlinedButton(onClick = { /*TODO*/ }) {
+                            Text(text = "I have an existing account")
+                        }
+
+                        ElevatedButton(
+                            onClick = { /*TODO*/ },
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Add,
+                                contentDescription = "Add to cart",
+                                modifier = Modifier.size(18.dp)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = "Add to cart")
+                        }
+                        FilledTonalButton(onClick = { /*TODO*/ }) {
+                            Text(text = "Open in browser")
+                        }
+                        OutlinedButton(onClick = { /*TODO*/ }) {
+                            Text(text = "Back")
+                        }
+                        TextButton(onClick = { /*TODO*/ }) {
+                            Text(text = "Learn more")
+                        }
+                    }
+                }
             }
         }
     }
