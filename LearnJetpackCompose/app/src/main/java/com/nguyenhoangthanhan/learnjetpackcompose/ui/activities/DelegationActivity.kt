684aa724548810cef49c4fdb8d75a64a106b6728
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-07 18:09:20 +0700

    Implemented coding delegation in DelegationActivity

---
+++ LearnJetpackCompose/app/src/main/java/com/nguyenhoangthanhan/learnjetpackcompose/ui/activities/DelegationActivity.kt
@@ -0,0 +1,12 @@
+package com.nguyenhoangthanhan.learnjetpackcompose.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nguyenhoangthanhan.learnjetpackcompose.R
+
+class DelegationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delegation)
+    }
+}
