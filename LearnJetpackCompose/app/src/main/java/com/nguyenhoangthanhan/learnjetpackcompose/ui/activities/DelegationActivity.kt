65b62ddb434c1c973a86894a6850654a3ce0b9a5
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-10 17:27:13 +0700

    Implemented additionally delegation examples

--- LearnJetpackCompose/app/src/main/java/com/nguyenhoangthanhan/learnjetpackcompose/ui/activities/DelegationActivity.kt
+++ LearnJetpackCompose/app/src/main/java/com/nguyenhoangthanhan/learnjetpackcompose/ui/activities/DelegationActivity.kt
@@ -14,6 +14,8 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import com.nguyenhoangthanhan.learnjetpackcompose.R
+import kotlin.properties.Delegates
+import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 class DelegationActivity : AppCompatActivity(), AnalyticsLogger by AnalyticsLoggerImpl(),
@@ -37,6 +39,38 @@ class DelegationActivity : AppCompatActivity(), AnalyticsLogger by AnalyticsLogg
 //        registerLifecycleOwner(this)
 
 //        println(obj)
+
+//        val bird = Bird()
+//        val chicken = Chicken()
+
+//        bird.makeSound()
+//        chicken.makeSound()
+//        println(chicken.name)
+
+//        chicken.officialName = "Putin Pro"
+//        println(chicken.officialName)
+
+//        bird.age = 20
+//        bird.age = 25
+
+//        bird.weight = 1.0
+//        println("DelegationActivity_TAG: Weight: ${bird.weight}")
+//        bird.weight = 2.5
+//        println("DelegationActivity_TAG: Weight: ${bird.weight}")
+
+        val bird = Bird(
+            mapOf(
+                "region" to "Europe",
+                "lifespan" to 2
+            )
+        )
+
+        println(
+            """
+            DelegationActivity_TAG: Lifespan: ${bird.lifespan} years
+            DelegationActivity_TAG: Region: ${bird.region}
+        """.trimIndent()
+        )
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -88,4 +122,110 @@ class AnalyticsLoggerImpl : AnalyticsLogger, LifecycleEventObserver {
         owner.lifecycle.addObserver(this)
     }
 
+}
+
+interface Animal {
+    val name: String
+    val canFly: Boolean
+    fun makeSound(): String
+    fun move(direction: String)
+}
+
+class Bird(
+    val birdDetails: Map<String, Any>
+) : Animal {
+    override val name: String
+        get() = "DelegationActivity_TAG: Bird"
+
+    @Deprecated("Use 'airborne' in stead", ReplaceWith("airborne"))
+    override val canFly: Boolean by this::airborne
+
+    val airborne: Boolean = true
+
+    val canSwim: Boolean by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        false
+    }
+
+    val region: String by birdDetails
+    val lifespan: Int by birdDetails
+
+    var age: Int by Delegates.observable(0) { property: KProperty<*>, oldValue: Int, newValue: Int ->
+        println(
+            """
+            DelegationActivity_TAG: ${property.name}
+            DelegationActivity_TAG: Old: $oldValue
+            DelegationActivity_TAG: New: $newValue
+        """.trimIndent()
+        )
+    }
+
+    var weight: Double by Delegates.vetoable(1.2) { property: KProperty<*>, oldValue: Double, newValue: Double ->
+        println(
+            """
+            DelegationActivity_TAG: ${property.name}
+            DelegationActivity_TAG: Old: $oldValue
+            DelegationActivity_TAG: New: $newValue
+        """.trimIndent()
+        )
+        oldValue < newValue
+    }
+
+    override fun makeSound(): String {
+        println("DelegationActivity_TAG: This bird crows")
+        return "DelegationActivity_TAG: Crows"
+    }
+
+    override fun move(direction: String) {
+        println("DelegationActivity_TAG: The bird is flying towards the $direction")
+    }
+}
+
+var birdAge: Int = 10
+
+class BirdMeasure(val weight: Double = 1.1)
+
+class Chicken(private var measure: BirdMeasure = BirdMeasure()) : Animal by Bird(mapOf()) {
+
+    override val name: String by birdNameDelegate()
+
+    var officialName: String by BirdName()
+
+    var age: Int by ::birdAge
+
+    val weight: Double by measure::weight
+
+    override fun makeSound(): String {
+        println("DelegationActivity_TAG: The Chicken is  crowing")
+        return "DelegationActivity_TAG: crows"
+    }
+
+    override fun toString(): String {
+        return """
+            This is a chicken1
+        """.trimIndent()
+    }
+}
+
+class BirdName {
+    operator fun getValue(classRef: Any?, property: KProperty<*>): String {
+        return "DelegationActivity_TAG: Bird: $classRef"
+    }
+
+    operator fun setValue(classRef: Any?, property: KProperty<*>, value: String) {
+        println("DelegationActivity_TAG: The $value property was delegated to $classRef")
+    }
+}
+
+fun birdNameDelegate(name: String = ""): ReadWriteProperty<Any?, String> {
+    return object : ReadWriteProperty<Any?, String>{
+        var currentName = name
+        override fun getValue(thisRef: Any?, property: KProperty<*>): String {
+            return "Bird: $currentName"
+        }
+
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+            currentName = value
+        }
+
+    }
 }
