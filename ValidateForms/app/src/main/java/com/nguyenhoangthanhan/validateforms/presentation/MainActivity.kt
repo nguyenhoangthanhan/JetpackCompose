ba646b1269bea8234676dad15b437f32cf9d4499
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-14 20:09:46 +0700

    Git implemented State Mapping, Submit

--- ValidateForms/app/src/main/java/com/nguyenhoangthanhan/validateforms/presentation/MainActivity.kt
+++ ValidateForms/app/src/main/java/com/nguyenhoangthanhan/validateforms/presentation/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.nguyenhoangthanhan.validateforms.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nguyenhoangthanhan.validateforms.ui.theme.ValidateFormsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +35,110 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ValidateFormsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val viewModel = viewModel<MainViewModel>()
+                    val state = viewModel.state
+                    val context = LocalContext.current
+                    LaunchedEffect(
+                        key1 = context,
+                        block = {
+                            viewModel.validationEvents.collect { event ->
+                                when (event) {
+                                    is MainViewModel.ValidationEvent.Success -> {
+                                        Toast.makeText(
+                                            context,
+                                            "Registration successful",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    }
+                                }
+                            }
+                        }
+                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(32.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        TextField(
+                            value = state.email,
+                            onValueChange = {
+                                viewModel.onEvent(RegistrationFormEvent.EmailChanged(it))
+                            },
+                            isError = state.emailError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Email")
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Email
+                            )
+                        )
+                        if (state.emailError != null) {
+                            Text(
+                                text = state.emailError,
+                                color = MaterialTheme.colorScheme.error
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
 
+                        TextField(
+                            value = state.password,
+                            onValueChange = {
+                                viewModel.onEvent(RegistrationFormEvent.PasswordChanged(it))
+                            },
+                            isError = state.passwordError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Password")
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Password
+                            ),
+                            visualTransformation = PasswordVisualTransformation()
+                        )
+                        if (state.passwordError != null) {
+                            Text(
+                                text = state.passwordError,
+                                color = MaterialTheme.colorScheme.error
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        TextField(
+                            value = state.repeatedPassword,
+                            onValueChange = {
+                                viewModel.onEvent(RegistrationFormEvent.RepeatedPasswordChanged(it))
+                            },
+                            isError = state.repeatedPasswordError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Re-Password")
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Password
+                            ),
+                            visualTransformation = PasswordVisualTransformation()
+                        )
+                        if (state.repeatedPasswordError != null) {
+                            Text(
+                                text = state.repeatedPasswordError,
+                                color = MaterialTheme.colorScheme.error
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Row (
+                            modifier = Modifier.fillMaxWidth()
+                        ){
+
+                        }
+                    }
+                }
             }
         }
     }
--- ValidateForms/app/src/main/java/com/nguyenhoangthanhan/validateforms/presentation/MainViewModel.kt
+++ ValidateForms/app/src/main/java/com/nguyenhoangthanhan/validateforms/presentation/MainViewModel.kt
@@ -2,11 +2,16 @@ package com.nguyenhoangthanhan.validateforms.presentation
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.nguyenhoangthanhan.validateforms.domain.use_case.ValidateEmail
 import com.nguyenhoangthanhan.validateforms.domain.use_case.ValidatePassword
 import com.nguyenhoangthanhan.validateforms.domain.use_case.ValidateRepeatedPassword
 import com.nguyenhoangthanhan.validateforms.domain.use_case.ValidateTerms
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val validateEmail: ValidateEmail = ValidateEmail(),
@@ -15,11 +20,72 @@ class MainViewModel(
     private val validateTerms: ValidateTerms = ValidateTerms()
 ) : ViewModel() {
 
-    val state by mutableStateOf(RegistrationFormState())
+    var state by mutableStateOf(RegistrationFormState())
+
+    private val validationEventChannel = Channel<ValidationEvent>()
+    val validationEvents = validationEventChannel.receiveAsFlow()
 
     fun onEvent(event: RegistrationFormEvent) {
         when (event) {
+            is RegistrationFormEvent.EmailChanged -> {
+                state = state.copy(
+                    email = event.email
+                )
+            }
 
+            is RegistrationFormEvent.PasswordChanged -> {
+                state = state.copy(
+                    password = event.password
+                )
+            }
+
+            is RegistrationFormEvent.RepeatedPasswordChanged -> {
+                state = state.copy(
+                    repeatedPassword = event.repeatedPassword
+                )
+            }
+
+            is RegistrationFormEvent.AcceptTerms -> {
+                state = state.copy(
+                    acceptTerms = event.isAccepted
+                )
+            }
+
+            is RegistrationFormEvent.Submit -> {
+                submitData()
+            }
         }
+    }
+
+    private fun submitData() {
+        val emailResult = validateEmail.execute(state.email)
+        val passwordResult = validatePassword.execute(state.password)
+        val repeatedPasswordResult =
+            validateRepeatedPassword.execute(state.password, state.repeatedPassword)
+        val termsResult = validateTerms.execute(state.acceptTerms)
+
+        val hasError = listOf(
+            emailResult,
+            passwordResult,
+            repeatedPasswordResult,
+            termsResult
+        ).any { !it.successful }
+
+        if (hasError) {
+            state = state.copy(
+                emailError = emailResult.errorMessage,
+                passwordError = passwordResult.errorMessage,
+                repeatedPasswordError = repeatedPasswordResult.errorMessage,
+                termsError = termsResult.errorMessage
+            )
+            return
+        }
+        viewModelScope.launch {
+            validationEventChannel.send(ValidationEvent.Success)
+        }
+    }
+
+    sealed class ValidationEvent {
+        object Success : ValidationEvent()
     }
 }
