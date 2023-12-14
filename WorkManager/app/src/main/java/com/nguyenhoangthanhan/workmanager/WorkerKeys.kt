5be5ce16697b3beda6e5120eac3b6cadd9d98695
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-15 00:31:51 +0700

    Add Download functions, Creating channel functions, Get API functions

---
+++ WorkManager/app/src/main/java/com/nguyenhoangthanhan/workmanager/WorkerKeys.kt
@@ -0,0 +1,7 @@
+package com.nguyenhoangthanhan.workmanager
+
+object WorkerKeys{
+    const val ERROR_MSG = "errorMsg"
+    const val IMAGE_URL = "imageUrl"
+    const val FILTER_URI = "filterUri"
+}
